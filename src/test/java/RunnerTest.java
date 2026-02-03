cee279967108be5ed339fe8981552e17d47b14d5
Avatar <ahmedhachemi1994@gmail.com>
2026-02-03 20:44:24 +0000

    fix

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -2,7 +2,18 @@ import com.intuit.karate.junit5.Karate;
 
 class RunnerTest {
     @Karate.Test
-    Karate testAll() {
-        return Karate.run().relativeTo(getClass());
-    }    
+    Karate exo1() {
+        return Karate.run("classpath:exo1.feature");
+    }
+
+    @Karate.Test
+    Karate exo2() {
+        return Karate.run("classpath:exo2.feature");
+    }
+
+    @Karate.Test
+    Karate exo3() {
+        return Karate.run("classpath:exo3.feature");
+    }
+    
 }
